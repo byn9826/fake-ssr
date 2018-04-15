00ca35aefc337ad5edc62d9f0c4ed437bcb018f4
byn9826 <byn9826@gmail.com>
2018-04-15 22:11:56 +0000

    auto detect url

--- src/FakeSSR.php
+++ src/FakeSSR.php
@@ -4,37 +4,38 @@ use Jaybizzle\CrawlerDetect\CrawlerDetect;
 
 class FakeSSR {
 	
-	public static function detect($address, $cache_path = null, $expire = false) {
+	public static function detect($cache_path = null, $expire = false) {
 		$CrawlerDetect = new CrawlerDetect;
     if($CrawlerDetect->isCrawler()) {
-			$html = self::render($address, $expire, $cache_path);
+			$html = self::render($expire, $cache_path);
 			echo $html;
 			exit;
 		}
-		
 	}
 	
-	private static function render($address, $expire, $cache_path) {
+	private static function render($expire, $cache_path) {
 		if ($expire === 0) {
-			return self::crawl($address, $expire, null);
+			return self::crawl($expire, null);
 		} else if ($expire !== 0) {
 			$file_path = $cache_path . '/__' . str_replace('/', '', $address) . '__.html';
-			if (!file_exists($file_path) || !self::check($file_path, $expire)) {
-				return self::crawl($address, $expire, $file_path);
-			} else {
-				return file_get_contents($file_path);
-			}
+			return self::check($file_path, $expire) ? 
+				file_get_contents($file_path) : self::crawl($expire, $file_path); 
 		}
 	}
 	
 	public static function check($file_path, $expire) {
+		if (!file_exists($file_path)) {
+			return false;
+		}
 		if (!$expire) {
 			return true;
 		}
 		return time() < filemtime($file_path) + $expire * 60;
 	}
 	
-	public static function crawl($address, $expire, $file_path) {
+	public static function crawl($expire, $file_path) {
+		$address = isset($_SERVER['HTTPS']) ? "https" : "http";
+		$address .= '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 		$cmd = 'google-chrome --headless --disable-gpu --dump-dom --no-sandbox --window-size=1280,1696 ';
 		$cmd .= $address . '; echo $?';
 		$content = shell_exec($cmd);
